066eb1628afc2d94b5c88d51f50e6fcc62c43153
lizanle <491823689@qq.com>
2019-03-18 18:12:22 +0800

    dubbometirc

---
+++ src/main/java/com/lzl/metric/Main.java
@@ -0,0 +1,11 @@
+package com.lzl.metric;
+
+/**
+ * @author lizanle
+ * @Date 2019/3/18 17:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        Bootstrap.init();
+    }
+}
