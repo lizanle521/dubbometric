36a818370a7f0a608fc373ce942441fab1a4d16f
lizanle <491823689@qq.com>
2019-03-19 17:02:06 +0800

    系统监控

--- src/main/java/com/lzl/Application.java
+++ src/main/java/com/lzl/Application.java
@@ -1,5 +1,7 @@
 package com.lzl;
 
+import com.lzl.metric.Bootstrap;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @data 2019/3/18 9:01 PM
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+
+    public void run(String... args) throws Exception {
+        Bootstrap.init();
     }
 }
--- src/main/java/com/lzl/interceptor/FrontHandlerInterceptor.java
+++ src/main/java/com/lzl/interceptor/FrontHandlerInterceptor.java
@@ -1,21 +1,32 @@
 package com.lzl.interceptor;
 
+import com.alibaba.metrics.FastCompass;
+import com.alibaba.metrics.MetricManager;
+import com.alibaba.metrics.MetricName;
 import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 /**
  * @author lizanle
  * @data 2019/3/18 9:10 PM
  */
 public class FrontHandlerInterceptor implements HandlerInterceptor {
-
+    ThreadLocal<Long> threadLocal = new ThreadLocal<Long>(){
+        @Override
+        protected Long initialValue() {
+            return System.currentTimeMillis();
+        }
+    };
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if(handler instanceof HttpRequestHandler){
-
+        if(handler instanceof HandlerMethod){
+            threadLocal.set(System.currentTimeMillis());
+            return true;
         }
         return false;
     }
@@ -25,6 +36,15 @@ public class FrontHandlerInterceptor implements HandlerInterceptor {
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+        if(handler instanceof HandlerMethod){
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Class<?> beanType = handlerMethod.getBeanType();
+            Method method = handlerMethod.getMethod();
+            FastCompass intercep = MetricManager.getFastCompass("dubbometric", MetricName.build(FrontHandlerInterceptor.class.getSimpleName()));
+            FastCompass hand = MetricManager.getFastCompass("dubbometric", MetricName.build(beanType.getSimpleName() + "#" + method.getName()));
+            long gap = System.currentTimeMillis()-threadLocal.get();
+            intercep.record(gap,response.getStatus()+"");
+            hand.record(gap,response.getStatus()+"");
+        }
     }
 }
